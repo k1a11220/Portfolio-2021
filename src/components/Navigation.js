dc084e775a022fae9b5d3b9c8190cbb23fc6e908
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-07-06 20:41:41 +0900

    Change navigation_bar design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,157 +1,201 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Hamburger.css";
 import styled from "@emotion/styled";
 
-const Wrapper = styled.nav`
-  width: 75rem; // 1200px
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  transition: all 0.5s ease-in-out;
-
-  @media screen and (max-width: 1400px) {
-    width: 90vw;
-  }
-
-  @media screen and (max-width: 768px) {
-    display: grid;
-    align-items: center;
-    position: relative;
-    width: 90vw;
+const Navbar = styled.nav`
+  position: sticky;
+  top: 0;
+  z-index: 50;
+  width: 100vw;
+  height: 64px;
+  border-bottom: 1px solid transparent;
+  background-color: #ffffff;
+  left: 0;
+  transition: background-color 0.2s ease;
+  border-bottom: solid 1px #ebebeb;
+  & a {
+    text-decoration: none;
   }
 `;
 
-const SelectArea = styled.div`
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1200px;
+  margin: auto;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  margin: auto;
+  @media (max-width: 1400px) {
+    width: 92%;
+  }
+  @media (max-width: 639px) {
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.div`
+  font-size: 20px;
+  font-weight: 500;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
-  transition: all 0.5s ease-in-out;
-
-  & p,
-  a {
-    font-size: 1rem; //16px
-    margin-left: 2.5rem; //40px
-    font-weight: 400;
-  }
-
-  @media screen and (max-width: 768px) {
-    display: none;
+  & a {
+    color: #1d1d1f;
   }
 `;
 
-const HamburgerContainer = styled.div`
+const ItemContainer = styled.div`
+  display: flex;
+  flex: 1;
+  overflow-y: hidden;
+  max-height: 100%;
+  justify-content: space-between;
+  transition: 0.44s 0.2s cubic-bezier(0.52, 0.16, 0.24, 1),
+    height 0.56s cubic-bezier(0.52, 0.16, 0.24, 1);
+
+  @media (max-width: 639px) {
+    width: 100%;
+    flex-direction: column;
+    position: absolute;
+    top: 64px;
+    left: 0;
+    max-height: 0;
+    border-bottom: solid 1px #ebebeb;
+
+    &.expanded {
+      max-height: 100vh;
+      height: auto;
+    }
+  }
+`;
+
+const StyledBurger = styled.button`
   display: none;
   flex-direction: column;
-  justify-content: flex-end;
-  transition: all 0.5s ease-in-out;
+  align-self: center;
+  justify-content: space-around;
+  width: 1.5rem;
+  height: 1.5rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
 
-  @media screen and (max-width: 768px) {
+  @media (max-width: 639px) {
     display: flex;
-    align-items: flex-end;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 1.5rem;
+    height: 0.125rem;
+    background: #1d1d1f;
+    border-radius: 10px;
+    transition: 0.44s 0.2s cubic-bezier(0.52, 0.16, 0.24, 1),
+      height 0.56s cubic-bezier(0.52, 0.16, 0.24, 1);
+    position: relative;
+    transform-origin: 1px;
+
+    :first-of-type {
+      transform: ${({ clicked }) => (clicked ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-of-type(2) {
+      opacity: ${({ clicked }) => (clicked ? "0" : "1")};
+      transform: ${({ clicked }) =>
+        clicked ? "translateX(30px)" : "translateX(0)"};
+    }
+
+    :nth-of-type(3) {
+      transform: ${({ clicked }) => (clicked ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  overflow-x: hidden;
+  padding: 0;
+  margin: 0;
+  background-color: #ffffff;
+  @media (max-width: 639px) {
+    flex-direction: column;
+  }
+`;
+
+const Item = styled.li`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  margin-left: 40px;
+
+  @media (max-width: 639px) {
+    width: 92%;
+    margin: 18px 0px;
+  }
+
+  & a {
+    font-size: 16px;
+    font-weight: 400;
+    transition: all 0.2s ease-in-out;
+    color: #1d1d1f;
+  }
+  & a:hover {
+    transition: all 0.2s ease-in-out;
+    color: #086bce;
   }
 `;
 
 const Navigation = () => {
-  const [hamburger, setHamburger] = useState(false);
-  const menuStatus = () => {
-    setHamburger(!hamburger);
+  const [clicked, setClicked] = useState(true);
+  const click = () => {
+    setClicked(!clicked);
   };
 
-  const Container = styled.section`
-    height: ${hamburger === false ? "4rem" : "20rem"}; //64px
-    display: flex;
-    justify-content: center;
-    background: rgba(255, 255, 255, 0.8);
-    backdrop-filter: blur(15px);
-    -webkit-backdrop-filter: blur(15px);
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0px;
-    z-index: 2;
-    transition: all 0.5s ease-in-out;
-
-    border-bottom: solid 2px #f3f3f3;
-  `;
-
-  const HamburgerMenu = styled.div`
-    display: ${hamburger === false ? "none" : "flex"};
-    flex-direction: column;
-    transition: all 0.5s ease-in-out;
-
-    & p,
-    a {
-      height: 3rem;
-    }
-  `;
-
-  const Label = styled.label`
-    display: flex;
-    flex-direction: column;
-    width: 30px;
-    cursor: pointer;
-
-    & span {
-      background: #222222;
-      border-radius: 10px;
-      height: 3px;
-      margin: 3px 0;
-      transition: 0.4s cubic-bezier(0.68, -0.6, 0.32, 1.6);
-    }
-
-    & span:nth-of-type(1) {
-      width: 50%;
-    }
-
-    & span:nth-of-type(2) {
-      width: 100%;
-    }
-
-    & span:nth-of-type(3) {
-      width: 75%;
-    }
-
-    & [type="checkbox"] {
-      display: none;
-    }
-  `;
-
   return (
-    <Container>
-      <Wrapper>
-        <>
-          <Link to="/" style={{ fontSize: "1.25rem", fontWeight: "600" }}>
-            Beomsoo Son
-          </Link>
-          <SelectArea>
-            <Link to="/">Work</Link>
-            <Link to="/About">About</Link>
-            <a href="https://www.beomsoo.me">Blog</a>
-            <a href="mailto: beskar.son@gmail.com">Contact</a>
-          </SelectArea>
-        </>
-        <HamburgerContainer>
-          <Label htmlFor="check">
-            <input type="checkbox" id="check" onClick={menuStatus} />
-            <span className={hamburger ? "bar1_checked" : ""}></span>
-            <span className={hamburger ? "bar2_checked" : ""}></span>
-            <span className={hamburger ? "bar3_checked" : ""}></span>
-          </Label>
-        </HamburgerContainer>
-        <HamburgerMenu>
-          <Link to="/" onClick={menuStatus}>
-            Work
-          </Link>
-          <Link to="/About" onClick={menuStatus}>
-            About
-          </Link>
-          <a href="https://www.beomsoo.me">Blog</a>
-          <a href="mailto: beskar.son@gmail.com">Contact</a>
-        </HamburgerMenu>
-      </Wrapper>
-    </Container>
+    <Navbar>
+      <Container>
+        <Wrapper>
+          <Logo>
+            <Link to="/"> Beomsoo Son </Link>
+          </Logo>
+          <ItemContainer className={clicked ? null : "expanded"}>
+            <ItemWrapper></ItemWrapper>
+            <ItemWrapper>
+              <Item>
+                <Link to="/about">About</Link>
+              </Item>
+              <Item>
+                <a
+                  href="https://blog.beomsoo.me"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Blog
+                </a>
+              </Item>
+              <Item>
+                <Link to="/">Contact</Link>
+              </Item>
+            </ItemWrapper>
+          </ItemContainer>
+          <StyledBurger clicked={!clicked} onClick={click}>
+            <div />
+            <div />
+            <div />
+          </StyledBurger>
+        </Wrapper>
+      </Container>
+    </Navbar>
   );
 };
 
