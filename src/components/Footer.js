1bbbe3891a106b73b051f4e53f9446b9060cda4b
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-07-06 20:27:52 +0900

    Change footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,74 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBehance,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-self: center;
   width: 100vw;
+  height: 148px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f1f2f4;
   margin-top: 40px;
-  background-color: #f3f3f3;
 `;
 
-const Wrapper = styled.ul`
-  width: 1200px;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  margin-top: 2.5rem;
-  align-items: flex-start;
-  justify-content: space-around;
-
-  @media screen and (max-width: 1400px) {
-    width: 90vw;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  @media (max-width: 1400px) {
+    width: 92%;
   }
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
-  }
-`;
 
-const Info = styled.div`
-  padding-right: 80px;
-
-  &:nth-child(2) {
-    display: flex;
+  @media (max-width: 768px) {
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
-  @media screen and (max-width: 768px) {
-    margin-bottom: 30px;
+`;
+
+const Cta = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #086bce;
+  padding: 8px 24px 8px 24px;
+  border-radius: 40px;
+  font-size: 0.875rem;
+  font-weight: 400;
+  color: #ffffff;
+
+  &:hover {
+    transform: scale(1.02);
   }
 
-  & h3 {
-    margin-bottom: 1rem;
+  @media (max-width: 768px) {
+    margin-bottom: 24px;
+  }
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  & svg {
+    margin-left: 16px;
+  }
+`;
+
+const Icon = styled(FontAwesomeIcon)``;
+
+const IconContainer = styled.a`
+  &:hover {
+    & > svg {
+      color: green;
+    }
   }
 `;
 
@@ -46,27 +76,43 @@ const Footer = () => {
   return (
     <Container>
       <Wrapper>
-        <Info>
-          <h3>Let’s make history together.</h3>
-          <a href="mailto:beskar.son@gmail.com">beskar.son@gmail.com</a>
-        </Info>
-        <Info>
-          <h3>Welcome to visit.</h3>
-          <a href="https://www.instagram.com/beomsoo_son/">Instagram</a>
-          <a href="https://www.behance.net/sonbeomsoo">Behance</a>
-          <a href="https://www.linkedin.com/in/beomsoo-son-65a201178/">
-            LinkedIn
-          </a>
-          <a href="https://github.com/k1a11220">Github</a>
-        </Info>
-        <p
-          style={{
-            fontSize: "12px",
-            marginBottom: "40px",
-          }}
+        <a
+          href="mailto:beskar.son@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Copyright © 2021 Beomsoo Son. All rights reserved.
-        </p>
+          <Cta>beskar.son@gmail.com</Cta>
+        </a>
+        <IconWrapper>
+          <IconContainer
+            href="https://www.behance.net/sonbeomsoo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon className="font-icon" icon={faBehance} size="lg" />
+          </IconContainer>
+          <IconContainer
+            href="https://www.instagram.com/beomsoo_son/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon icon={faInstagram} size="lg" />
+          </IconContainer>
+          <IconContainer
+            href="https://github.com/k1a11220"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon icon={faGithub} size="lg" />
+          </IconContainer>
+          <IconContainer
+            href="https://www.linkedin.com/in/beomsoo-son-65a201178/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon icon={faLinkedinIn} size="lg" />
+          </IconContainer>
+        </IconWrapper>
       </Wrapper>
     </Container>
   );
